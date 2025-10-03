ea59beceb05bc109ba9792a0bb4ab5ae957cdc1c
Akkie <g2453765@cc.kyoto-su.ac.jp>
2025-10-03 12:19:06 +0900

    feat : 2カラムの基本レイアウトを追加
    - bodyにSidebarとmain要素を配置
    - bodyにGridを適用し、2カラムレイアウトを実装
    - main要素内をflexboxで縦並びに設定

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Sidebar from "@/components/Sidebar/";
+
+import "./style.css";
+
+
 export default function Home() {
   return (
     <>
+    <Sidebar />
+    <main></main>
     </>
   );
 }
