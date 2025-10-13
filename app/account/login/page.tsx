ee42494ee960061404ff2ae6c3a9c8890a48a97d
Oshota501 <kakidesu1111@gmail.com>
2025-10-13 16:10:55 +0900

    ログイン画面作った、サインアップも

---
+++ app/account/login/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import "./style.css" ;
+import Image from 'next/image';
+
+export default function Page(){
+    return (
+        <div className="login">
+            <Image
+                src="/img/logo.png" // publicフォルダからのパス
+                alt="Logo"
+                width={800} // 画像の元の幅を指定
+                height={160} // 画像の元の高さを指定
+            />
+            <h1>ログイン</h1>
+            {/* apiと連携 */}
+            <form method="post" action="/">
+                <p>ユーザー名：<input name="username" required /></p>
+                <p>パスワード：<input name="password" type="password" required /></p>
+                <p><button type="submit">ログイン</button></p>
+            </form>
+
+            <br>
+            </br>
+            <Link href="./signUP">アカウントをお持ちでない方：新しいアカウントを作る</Link>
+        </div>
+    )
+}
---
+++ app/account/signUP/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import "./style.css" ;
+import Image from 'next/image';
+
+export default function Page(){
+    return (
+        <div className="login">
+            <Image
+                src="/img/logo.png" // publicフォルダからのパス
+                alt="Logo"
+                width={800} // 画像の元の幅を指定
+                height={160} // 画像の元の高さを指定
+            />
+            <h1>サインアップ</h1>
+            {/* apiと連携 */}
+            <form method="post" action="/">
+                <p>ユーザー名：<input name="username" required /></p>
+                <p>メールアドレス：<input name="mail" type="main" required /></p>
+                <p>パスワード：<input name="password" type="password" required /></p>
+                <p><button type="submit">登録</button></p>
+            </form>
+            <br></br>
+            <Link href="./login">すでにアカウントをお持ちの方：ログイン</Link>
+        </div>
+    )
+}
