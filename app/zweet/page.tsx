c2b3eb9ef729ebc9fe2087224d6cd8e1c12b13cd
Oshota501 <kakidesu1111@gmail.com>
2025-10-13 15:38:40 +0900

    画面作成、モックデータ表示

--- app/zweet/page.tsx
+++ app/zweet/page.tsx
@@ -2,18 +2,31 @@ import Sidebar from "@/components/Sidebar"
 import "./style.css";
 import mockLabels from '@/types/mock'
 import AllZweetScreen from "./main";
+import Image from 'next/image';
 
 export default function AllZweetScreeb(){
     // モックデータを挿入
     // 本番環境で要変更
     const { d } = mockLabels;
     return (
-        <div className="main">
-            <Sidebar/>
-            <div className="center-labels">
-                <AllZweetScreen 
-                    d = {d}
-                />
+        <div className="mainPage">
+            <div className="sidebar-loginInfo">
+                <Sidebar/>
+            </div>
+            <div className="mainbar-zweetInfo">
+                <div className="logo">
+                    <Image
+                        src="/img/logo.png" // publicフォルダからのパス
+                        alt="Logo"
+                        width={600} // 画像の元の幅を指定
+                        height={120} // 画像の元の高さを指定
+                    />
+                </div>
+                <div className="center-labels">
+                    <AllZweetScreen 
+                        d = {d}
+                    />
+                </div>
             </div>
         </div>
     )
--- components/post/user.tsx
+++ components/post/user.tsx
@@ -1,11 +1,15 @@
 import { UserType } from "@/types/user";
+import Image from 'next/image'
 import './user.css';
 
 export default function User({id,name,ico}:UserType){
+    const src = ico || '/favicon.ico'
     return (
-        <div className="user">
-            <div className="user-icon"></div>
+        <span className="user">
+            <span className="user-icon">
+                <Image src={src} alt={`${name} icon`} width={16} height={16} />
+            </span>
             {name}
-        </div>
+        </span>
     )
 }
