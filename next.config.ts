05c55fe51665ef3b0571c33f19da0a004bd2de5b
Nikoyaka9dd <minikkosumikko@gmail.com>
2025-11-03 00:22:56 +0900

    デプロイするための設定を追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // サイトを静的ファイルとして出力(export)する設定
+  output: 'export',
+  
+  // サイトのURLが /z_channel/ になるための設定
+  basePath: '/z_channel',
+  assetPrefix: '/z_channel/',
 };
 
 export default nextConfig;
